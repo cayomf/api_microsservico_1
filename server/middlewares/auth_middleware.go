596dfff22eb6e1f2692c82faa9f19cf9ec279be5
Cayo Cesar Maia Fonseca <cayomf@gmail.com>
2023-04-03 03:36:27 -0300

    correcao code smell

--- server/middlewares/auth_middleware.go
+++ server/middlewares/auth_middleware.go
@@ -7,13 +7,13 @@ import (
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		const Bearer_schema = "Bearer "
+		const bearer_schema = "Bearer "
 		header := c.GetHeader("Authorization")
 		if header == "" {
 			c.AbortWithStatus(401)
 		}
 
-		token := header[len(Bearer_schema):]
+		token := header[len(bearer_schema):]
 
 		if !services.NewJWTService().ValidateToken(token) {
 			c.AbortWithStatus(401)
